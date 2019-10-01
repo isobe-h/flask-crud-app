def70ef5912978508dcb3a81e634728362a90631
isobe-h <isobe-h@toack.co.jp>
2019-10-01 11:08:38 +0900

    init

---
+++ flask_blog/config.py
@@ -0,0 +1,7 @@
+# flask_ blog/ config. py
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DEBUG = True
+SECRET_KEY = "secret_key"
+USERNAME = "clair"
+PASSWORD = "due123"
---
+++ flask_blog/views/views.py
@@ -0,0 +1,24 @@
+# flask_ blog/ views. py
+from flask_blog import app
+from flask import request, redirect, url_for, render_template, flash, session
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            flash("ユーザ名が異なる")
+        elif request.form["password"] != app.config["PASSWORD"]:
+            flash("パスワードが異なる")
+        else:
+            session["logged_in"] = True
+            flash("ログインしました")
+            return redirect(url_for("show_entries"))
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("ログアウトしました")
+    return redirect(url_for("show_entries"))
